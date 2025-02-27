de22fbd5f99d70325698f9cec816712d99987eac
adam vietro <adam.vietro@gmail.com>
2025-02-26 17:42:20 -0800

    Added validation for the Posts data structure in regards to both unique title and a test to be sure that the post most be in the past and not in the future. There is still an issue as there is 24hr in a day and the UTC-today isnt catching things that are within 12 hours in the future. Still need to add the route for the comments.

--- lib/blog/posts/post.ex
+++ lib/blog/posts/post.ex
@@ -1,6 +1,7 @@
 defmodule Blog.Posts.Post do
   use Ecto.Schema
   import Ecto.Changeset
+  import EctoCommons.DateValidator
 
   schema "posts" do
     field :title, :string
@@ -18,5 +19,8 @@ defmodule Blog.Posts.Post do
     post
     |> cast(attrs, [:title, :content, :published_on, :visibility])
     |> validate_required([:title, :content, :published_on, :visibility])
+    |> unsafe_validate_unique(:title, Blog.Repo)
+    |> unique_constraint(:title)
+    |> validate_date(:published_on, before: :utc_today, message: "Shouldn't be in the Future")
   end
 end
--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Blog.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:ecto_commons, "~> 0.3.4"}
     ]
   end
 
