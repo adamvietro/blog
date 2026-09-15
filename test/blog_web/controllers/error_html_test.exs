76e4352063f49d55a9c6500f59ddef771beb6f04
adam vietro <hunterafi@gmail.com>
2026-09-15 12:17:45 -0700

    Favicon/Error Page Changed the favicon to reflect my page,
    Also changed the error pages to be my pages.

--- test/blog_web/controllers/error_html_test.exs
+++ test/blog_web/controllers/error_html_test.exs
@@ -4,11 +4,29 @@ defmodule BlogWeb.ErrorHTMLTest do
   # Bring render_to_string/4 for testing custom views
   import Phoenix.Template
 
-  test "renders 404.html" do
-    assert render_to_string(BlogWeb.ErrorHTML, "404", "html", []) == "Not Found"
+  test "renders a themed 404 page" do
+    html = render_to_string(BlogWeb.ErrorHTML, "404", "html", [])
+
+    assert html =~ "404"
+    assert html =~ "page not found"
+    assert html =~ "Back home"
   end
 
-  test "renders 500.html" do
-    assert render_to_string(BlogWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+  test "renders a themed 500 page" do
+    html = render_to_string(BlogWeb.ErrorHTML, "500", "html", [])
+
+    assert html =~ "500"
+    assert html =~ "internal server error"
+    assert html =~ "Back home"
+  end
+
+  test "falls back to plain text for a status with no template" do
+    assert render_to_string(BlogWeb.ErrorHTML, "403", "html", []) == "Forbidden"
+  end
+
+  test "GET on an unmatched route returns the themed 404 page", %{conn: conn} do
+    conn = get(conn, "/this-page-does-not-exist")
+
+    assert html_response(conn, 404) =~ "Back home"
   end
 end
